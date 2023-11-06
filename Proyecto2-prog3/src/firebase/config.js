885ad951ecb29fd631ac990e85b70b9dedc7c529
TomasHarten <thartenstein@udesa.edu.ar>
2023-11-06 15:49:14 -0300

    Instale firebase

---
+++ Proyecto2-prog3/src/firebase/config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDRgp-vcfdRKg5Y6drBQFvXLUBo1TKdPHw",
+    authDomain: "proyect2-prog3.firebaseapp.com",
+    projectId: "proyect2-prog3",
+    storageBucket: "proyect2-prog3.appspot.com",
+    messagingSenderId: "329151135303",
+    appId: "1:329151135303:web:d3c2d7a911a2308810bb62"
+  };
